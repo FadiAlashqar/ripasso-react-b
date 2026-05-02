d648bb45eda3f214b326690cc8005be49aeca77f
FadiAlashqar <fadialshqar.fa01@gmail.com>
2026-05-02 15:55:38 +0200

    review exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [show, setShow] = useState(false)
+
+  const handleButton = () => {
+    setShow(true)
+    setTimeout(() => {
+      setShow(false)
+      setName("")
+    }, 2000)
+  }
 
   return (
-    <div>
-      <h1>{count}</h1>
+    // <div>
+    //   <h1>{count}</h1>
 
-      <button onClick={() => setCount(prev => prev + 1)}>+</button>
-      <button onClick={() => setCount(prev => prev - 1)}>-</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+    //   <button onClick={() => setCount(prev => prev + 1)}>+</button>
+    //   <button onClick={() => setCount(prev => prev - 1)}>-</button>
+    //   <button onClick={() => setCount(0)}>Reset</button>
+    // </div>
+    <div>
+      <input type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <button onClick={handleButton}>Saluta</button>
+      {show && <h2>Ciao {name}!</h2>}
     </div>
   );
 }
